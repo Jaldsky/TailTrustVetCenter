753e013a86b1d2ae03899c1a9c6e3cede456f01e
NordN <RyzhenkovArt@yandex.ru>
2024-03-16 20:37:21 +0500

    Modify models

--- app/models.py
+++ app/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 
 class Client(models.Model):
+    telegram_chat_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
     phone = models.CharField(max_length=15)
-    telegram_chat_id = models.IntegerField(unique=True)
 
 
 class Appointment(models.Model):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    appointment_datetime = models.DateTimeField()
-    animal_type = models.CharField(max_length=255)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, to_field='telegram_chat_id')
+    date = models.DateField(default=None, null=True)
+    time = models.TimeField(default=None, null=True)
+    pet_type = models.CharField(max_length=255)
